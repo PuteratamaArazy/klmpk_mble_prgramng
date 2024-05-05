f907e5fe520eef989c01abba2cdee5fc7dff7397
Puteratama.K.Arazy <34960152+PuteratamaArazy@users.noreply.github.com>
2024-05-06 00:29:19 +0700

    Update sidebar.dart

--- lib/widget/sidebar.dart
+++ lib/widget/sidebar.dart
@@ -14,7 +14,9 @@ class Sidebar extends StatelessWidget {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          UserAccountsDrawerHeader(
+          const UserAccountsDrawerHeader(
+            accountName: Text('Nama User'),
+            accountEmail: Text('user@example.com'),
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 begin: Alignment.topLeft,
@@ -22,36 +24,38 @@ class Sidebar extends StatelessWidget {
                 colors: [Colors.purple, Colors.red],
               ),
             ),
-            accountName: const Text("Syade", style: TextStyle(color: Colors.white)),  // Ensure text is white
-            accountEmail: const Text("irsyadmuhammad020@gmail.com", style: TextStyle(color: Colors.white)),  // Ensure text is white
           ),
           _createDrawerItem(
             icon: Icons.home,
             text: "Beranda",
-            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const Beranda())),
-            textColor: Colors.teal,  // Specify text color
-            iconColor: Colors.teal,  // Specify icon color
+            onTap: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const Beranda())),
+            textColor: Colors.teal, // Specify text color
+            iconColor: Colors.teal, // Specify icon color
           ),
           _createDrawerItem(
             icon: Icons.accessible,
             text: "Poli",
-            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const PoliPage())),
-            textColor: Colors.teal,  // Specify text color
-            iconColor: Colors.teal,  // Specify icon color
+            onTap: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const PoliPage())),
+            textColor: Colors.teal, // Specify text color
+            iconColor: Colors.teal, // Specify icon color
           ),
           _createDrawerItem(
             icon: Icons.people,
             text: "Pegawai",
-            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => PegawaiPage())),
-            textColor: Colors.teal,  // Specify text color
-            iconColor: Colors.teal,  // Specify icon color
+            onTap: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => PegawaiPage())),
+            textColor: Colors.teal, // Specify text color
+            iconColor: Colors.teal, // Specify icon color
           ),
           _createDrawerItem(
             icon: Icons.account_box_sharp,
             text: "Pasien",
-            onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => PasienPage())),
-            textColor: Colors.teal,  // Specify text color
-            iconColor: Colors.teal,  // Specify icon color
+            onTap: () => Navigator.push(
+                context, MaterialPageRoute(builder: (context) => PasienPage())),
+            textColor: Colors.teal, // Specify text color
+            iconColor: Colors.teal, // Specify icon color
           ),
           _createDrawerItem(
             icon: Icons.logout_rounded,
@@ -59,24 +63,27 @@ class Sidebar extends StatelessWidget {
             onTap: () => Navigator.pushAndRemoveUntil(
               context,
               MaterialPageRoute(builder: (context) => const Login()),
-                  (Route<dynamic> route) => false,
+              (Route<dynamic> route) => false,
             ),
-            textColor: Colors.teal,  // Specify text color
-            iconColor: Colors.teal,  // Specify icon color
+            textColor: Colors.teal, // Specify text color
+            iconColor: Colors.teal, // Specify icon color
           ),
         ],
       ),
     );
   }
 
-  Widget _createDrawerItem({required IconData icon, required String text, required VoidCallback onTap, required Color textColor, required Color iconColor}) {
+  Widget _createDrawerItem(
+      {required IconData icon,
+      required String text,
+      required VoidCallback onTap,
+      required Color textColor,
+      required Color iconColor}) {
     return ListTile(
       leading: Icon(icon, color: iconColor), // Adjust icon color to specified
-      title: Text(text, style: TextStyle(color: textColor)), // Adjust text color to specified
+      title: Text(text,
+          style: TextStyle(color: textColor)), // Adjust text color to specified
       onTap: onTap,
     );
   }
 }
-
-
-
